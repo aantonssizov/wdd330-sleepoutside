0aafeadac9947bef1aa6094bda10fe4f83a4a106
Anton Syzov <aantonssizov@gmail.com>
2026-01-23 19:50:49 +0100

    fix products title bug

--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -2,16 +2,20 @@ import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
 import { loadHeaderFooter, getParam, loadLocalJSON } from "./utils.mjs";
 
-loadHeaderFooter();
+async function setProductsTitle() {
+  const categoryNames = await loadLocalJSON("category-names.json");
+  const categoryName = categoryNames[category];
+  const productsTitle = document.querySelector("section.products h2");
 
-const categoryNames = await loadLocalJSON("category-names.json");
+  productsTitle.textContent = `Top Products: ${categoryName}`;
+}
+
+loadHeaderFooter();
+setProductsTitle();
+
 const category = getParam("category");
-const categoryName = categoryNames[category];
-const productsTitle = document.querySelector("section.products h2");
 const productsSection = document.querySelector("ul.product-list");
 const dataSource = new ProductData(category);
 const products = new ProductList(category, dataSource, productsSection);
-
-productsTitle.textContent = `Top Products: ${categoryName}`;
 
 products.init();
