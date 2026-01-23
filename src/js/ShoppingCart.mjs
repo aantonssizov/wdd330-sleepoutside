13adceacd3b212be1e1e214476978017136fd207
Anton Syzov <aantonssizov@gmail.com>
2026-01-23 21:08:21 +0100

    Remove from cart feature

--- src/js/ShoppingCart.mjs
+++ src/js/ShoppingCart.mjs
@@ -1,10 +1,10 @@
-import { renderListWithTemplate } from "./utils.mjs";
+import { renderListWithTemplate, setLocalStorage } from "./utils.mjs";
 
 function cartItemTemplate(item) {
-    const newItem = `<li class="cart-card divider">
+  const newItem = `<li class="cart-card divider">
   <a href="#" class="cart-card__image">
     <img
-      src="${item.Image}"
+      src="${item.Images.PrimaryMedium}"
       alt="${item.Name}"
     />
   </a>
@@ -14,18 +14,26 @@ function cartItemTemplate(item) {
   <p class="cart-card__color">${item.Colors[0].ColorName}</p>
   <p class="cart-card__quantity">qty: 1</p>
   <p class="cart-card__price">$${item.FinalPrice}</p>
+  <button class="card__remove" data-id="${item.Id}">X</button>
 </li>`;
 
-    return newItem;
+  return newItem;
 }
 
 export default class ShoppingCart {
-    constructor(cartItems, listElement) {
-        this.cartItems = cartItems;
-        this.listElement = listElement;
-    }
+  constructor(cartItems, listElement) {
+    this.cartItems = cartItems;
+    this.listElement = listElement;
+  }
 
-    renderList() {
-        renderListWithTemplate(cartItemTemplate, this.listElement, this.cartItems, "afterbegin", true);
-    }
+  renderList() {
+    renderListWithTemplate(cartItemTemplate, this.listElement, this.cartItems, "afterbegin", true);
+    this.listElement.querySelectorAll("button.card__remove").forEach(button => {
+      button.addEventListener("click", () => {
+        this.cartItems = this.cartItems.filter(cartItem => cartItem.Id !== button.getAttribute("data-id"));
+        setLocalStorage("so-cart", this.cartItems);
+        this.renderList();
+      })
+    })
+  }
 }
