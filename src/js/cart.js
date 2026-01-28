15e3850aa7b3d78337473b84aae4b5c08556f6c0
Anton Syzov <aantonssizov@gmail.com>
2026-01-28 23:43:33 +0100

    npm format

--- src/js/cart.js
+++ src/js/cart.js
@@ -3,14 +3,16 @@ import { getLocalStorage } from "./utils.mjs";
 import { loadHeaderFooter } from "./utils.mjs";
 
 function setCartFooter() {
-    const cartFooter = document.querySelector(".cart-footer");
-    const cartTotal = cartFooter.querySelector("p.cart-total");
+  const cartFooter = document.querySelector(".cart-footer");
+  const cartTotal = cartFooter.querySelector("p.cart-total");
 
-    if (!cartItems.length) return;
+  if (!cartItems.length) return;
 
-    cartFooter.classList.remove("hide");
-    const total = cartItems.reduce((prev, curr) => prev + curr.FinalPrice, 0).toFixed(2);
-    cartTotal.textContent = `Total: ${total}$`;
+  cartFooter.classList.remove("hide");
+  const total = cartItems
+    .reduce((prev, curr) => prev + curr.FinalPrice, 0)
+    .toFixed(2);
+  cartTotal.textContent = `Total: ${total}$`;
 }
 
 const cartItems = getLocalStorage("so-cart");
