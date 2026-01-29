1a1bd8873c53c449eac21ffa575cb2ccaeba04a8
Anton Syzov <aantonssizov@gmail.com>
2026-01-29 19:51:59 +0100

    W04 team activity

--- src/js/CheckoutProcess.mjs
+++ src/js/CheckoutProcess.mjs
@@ -1,8 +1,36 @@
+import ExternalServices from "./ExternalServices.mjs";
 import { getLocalStorage } from "./utils.mjs";
+
+function packageItems(items) {
+    const result = items.map((item) => ({
+        id: item.Id,
+        name: item.Name,
+        price: item.FinalPrice,
+        quantity: 1
+    }))
+
+    return result;
+}
+
+function formDataToJSON(formElement) {
+    const formData = new FormData(formElement),
+        convertedJSON = {};
+
+    formData.forEach(function (value, key) {
+        convertedJSON[key] = value;
+    });
+
+    return convertedJSON;
+}
 
 export default class CheckoutProcess {
     constructor() {
         this.cartItems = getLocalStorage("so-cart");
+        this.subtotal = 0;
+        this.tax = 0;
+        this.shipping = 0;
+        this.orderTotal = 0;
+        this.externalServices = new ExternalServices();
     }
 
     setOrderSubtotal() {
@@ -10,29 +38,36 @@ export default class CheckoutProcess {
 
         if (!this.cartItems.length) return;
 
-        const subtotal = this.cartItems
-            .reduce((prev, curr) => prev + curr.FinalPrice, 0);
+        this.subtotal = this.cartItems
+            .reduce((prev, curr) => prev + curr.FinalPrice, 0).toFixed(2);
 
-        orderSubtotal.textContent = `Subtotal: $${subtotal.toFixed(2)}`;
+        orderSubtotal.textContent = `Subtotal: $${this.subtotal}`;
     }
 
     setOrderSummary() {
-        const orderSubtotal = document.querySelector("p.subtotal");
         const orderTax = document.querySelector("p.tax")
         const orderShippingEstimate = document.querySelector("p.shipping-estimate");
-        const orderTotal = document.querySelector("p.order-total");
+        const orderTotalElm = document.querySelector("p.order-total");
 
         if (!this.cartItems.length) return;
 
-        const subtotal = this.cartItems
-            .reduce((prev, curr) => prev + curr.FinalPrice, 0);
-        const tax = (subtotal * 0.06); // sales tax is 6%
-        const shipping = (this.cartItems.length - 1) * 2 + 10; // 10$ for the first item + 2$ for each additional item
-        const total = +subtotal + +tax + +shipping;
+        this.tax = (this.subtotal * 0.06).toFixed(2); // sales tax is 6%
+        this.shipping = (this.cartItems.length - 1) * 2 + 10; // 10$ for the first item + 2$ for each additional item
+        this.orderTotal = (+this.subtotal + +this.tax + +this.shipping).toFixed(2);
 
-        orderSubtotal.textContent = `Subtotal: $${subtotal.toFixed(2)}`;
-        orderTax.textContent = `Tax: $${tax.toFixed(2)}`;
-        orderShippingEstimate.textContent = `Shipping Estimate: $${shipping.toFixed(2)}`;
-        orderTotal.textContent = `Order Total: $${total.toFixed(2)}`;
+        orderTax.textContent = `Tax: $${this.tax}`;
+        orderShippingEstimate.textContent = `Shipping Estimate: $${this.shipping}`;
+        orderTotalElm.textContent = `Order Total: $${this.orderTotal}`;
+    }
+
+    async checkout(form) {
+        const payload = formDataToJSON(form);
+        payload["items"] = packageItems(this.cartItems);
+        payload["orderDate"] = new Date().toISOString();
+        payload["orderTotal"] = this.orderTotal;
+        payload["shipping"] = this.shipping;
+        payload["tax"] = this.tax;
+
+        await this.externalServices.checkout(payload);
     }
 }
--- src/js/checkout.js
+++ src/js/checkout.js
@@ -3,12 +3,18 @@ import CheckoutProcess from "./CheckoutProcess.mjs";
 
 const checkoutProcess = new CheckoutProcess();
 const zipInput = document.querySelector("input#zipCode");
+const checkoutForm = document.querySelector("form.checkout-form");
 
 loadHeaderFooter();
 checkoutProcess.setOrderSubtotal();
 
-zipInput.addEventListener("input", (event) => {
-  if (!event.data) return;
+zipInput.addEventListener("input", () => {
+  if (!zipInput.value) return;
 
   checkoutProcess.setOrderSummary();
+});
+
+checkoutForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  checkoutProcess.checkout(checkoutForm);
 });
